a365ebfe06018f2ce55b56ea6b4f2221c60db8b4
Alex HM <alex13pks@gmail.com>
2023-06-27 22:22:31 -0600

    Added github and readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# DritsJS - Playground
+
+## Practice and code everywhere
+
+A simple JavaScript playground to code, learn, teach, and create algorithms with a realtime output! 👨‍💻
+Use the split grid to focus on the code or the output! ⚡️
+
+## Open source
+
+Feel free to fork the repo and make any issues if you find or miss anything!
+
+## Upcoming features
+
+- Save and upload files
+- Better error catching
+- Rename tabs
+- Desktop support (MacOS/Windows)
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -191,10 +191,31 @@ function Playground() {
 
   return (
     <div className="min-h-screen items-center justify-center bg-[#0b1015] text-white">
-      <div className="flex justify-center items-center">
+      <header className="flex justify-center items-center">
         <img src="/logoRED.svg" alt="logo" className="w-9" />
         <h1 className="text-2xl font-bold">DritsJS - Playground</h1>
-      </div>
+        <a
+          href="https://github.com/alexhmdev/js-playground"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-brand-github"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+          </svg>
+        </a>
+      </header>
       <nav className="flex border-b-[1px] border-slate-500">
         {tabs.map((tab, index) => (
           <Tab
